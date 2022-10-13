fd76df367c08c601140260ecca5e7431446a1c8a
Richard Burleigh <richard@richardburleigh.com.au>
2022-10-13 16:22:07 +0900

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -10,6 +10,12 @@ setup(
     description="A simple Python tool to extract object sizes from Mendix MPR files and export them into Excel reports.",
     author="Richard Burleigh <richard.burleigh@mendix.com>",
     packages=find_packages(),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ],
     entry_points = {
         'console_scripts': ['mprsize=mprsize.analyzer:cli'],
     },
